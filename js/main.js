833579aefa953d26523bfca29b9c057df7d99f0b
Сергей <trutnevss92@gmail.com>
2021-10-10 16:57:40 +1000

    Генератор списка объявлений

--- js/main.js
+++ js/main.js
@@ -1,36 +1,80 @@
-function isPositiveNumbers(min, max) {
-  return min >= 0 && max >= 0;
+function getRandomPositiveFloat (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
 }
 
-function isIntExist(min, max) {
-  return Number.isInteger(min) || Number.isInteger(max);
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-function getRandomIntInclusive(min, max) {
+const avatarsNumbers = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
 
-  if (!isIntExist(min, max) || !isPositiveNumbers(min, max)) {
-    return 'Не корректный диапазон';
-  }
+function getAuthorAvatar() {
 
-  if (min === max) {
-    return min;
-  }
+  const AuthorAvatar = {
+    avatar : `img/avatars/user${  avatarsNumbers.splice(getRandomPositiveInteger(0, avatarsNumbers.length - 1), 1)  }.png`,
+  };
 
-  if (min > max) {
-    return Math.floor(Math.random() * (min - max + 1)) + max;
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return AuthorAvatar;
 }
 
-function getRandomFloatInclusive(min, max,  num) {
+function getOfferInformation(location) {
 
-  if (!isPositiveNumbers(min, max) || min > max || min === max) {
-    return 'Не корректный диапазон';
-  }
+  const OfferInformation = {
+    title : 'Заголовок',
+    adress : `${location.lat  }, ${  location.lng}`,
+    price : Number(Math.random().toFixed(7).substring(2)),
+    type : ['palace', 'flat', 'house', 'bungalow', 'hotel'][getRandomPositiveInteger(0, 4)],
+    rooms : Number(Math.random().toFixed(1).substring(2)),
+    guests : Number(Math.random().toFixed(1).substring(2)),
+    checkin : ['12:00', '13:00', '14:00'][getRandomPositiveInteger(0, 2)],
+    checkout : ['12:00', '13:00', '14:00'][getRandomPositiveInteger(0, 2)],
+    features : ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'].splice(getRandomPositiveInteger(0, 4), getRandomPositiveInteger(1, 5)),
+    description : 'описание',
+    photos : ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'].splice(getRandomPositiveInteger(0, 2), getRandomPositiveInteger(1, 3)),
+  };
 
-  return (Math.random() * (max - min) + min).toFixed(num);
+  return OfferInformation;
 }
 
-getRandomIntInclusive(0, 10);
-getRandomFloatInclusive(0, 10);
+function getLocationCoordinates() {
+
+  const LocationCoordinates = {
+    lat : getRandomPositiveFloat(35.65000, 35.70000, 5),
+    lng : getRandomPositiveFloat(139.70000, 139.80000, 5),
+  };
+
+  return LocationCoordinates;
+}
+
+function getAdsItem() {
+
+  const location = getLocationCoordinates();
+
+  const adsItem = {
+    author : getAuthorAvatar(),
+    location : location,
+    offer : getOfferInformation(location),
+  };
+
+  return adsItem;
+}
+
+function getAdsList() {
+
+  const adsList = [];
+
+  // eslint-disable-next-line id-length
+  for (let i = 0; i < 10; i++) {
+    adsList.push(getAdsItem());
+  }
+
+  return adsList;
+}
+
+getAdsList();
