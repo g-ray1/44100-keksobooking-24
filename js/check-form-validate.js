f09b8a9ad83e279d6c2458432f6930358609e6f8
Сергей <trutnevss92@gmail.com>
2021-11-12 20:10:36 +1000

    Доделал валидацию

--- js/check-form-validate.js
+++ js/check-form-validate.js
@@ -1,6 +1,19 @@
 const roomsSelect = document.querySelector('#room_number');
 const capacitySelect = document.querySelector('#capacity');
 const capacitySelectOptions = capacitySelect.querySelectorAll('option');
+const typeSelect = document.querySelector('#type');
+const priceInput = document.querySelector('#price');
+const timeInSelect = document.querySelector('#timein');
+const timeOutSelect = document.querySelector('#timeout');
+const timeOutSelectOptions = timeOutSelect.querySelectorAll('option');
+
+const allTypes = {
+  bungalow : 0,
+  flat : 1000,
+  hotel : 3000,
+  house : 5000,
+  palace : 10000,
+};
 
 roomsSelect.addEventListener('change', (evt) => {
   capacitySelectOptions.forEach((option) => {
@@ -9,6 +22,24 @@ roomsSelect.addEventListener('change', (evt) => {
     } else {
       option.disabled = false;
       option.setAttribute('selected', true);
+    }
+  });
+});
+
+typeSelect.addEventListener('click', (evt) => {
+  const selectType = evt.target.value;
+  priceInput.min = allTypes[selectType];
+  priceInput.placeholder = allTypes[selectType];
+});
+
+timeInSelect.addEventListener('change', (evt) => {
+  timeOutSelectOptions.forEach((option) => {
+    if (option.value === evt.target.value) {
+      option.selected = true;
+      option.disabled = false;
+    } else {
+      option.selected = false;
+      option.disabled = true;
     }
   });
 });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import './fill-ads-template.js';
 import './check-form-validate.js';
-import './togle-page-state.js';
 import { diactivatePage, activatePage } from './togle-page-state.js';
 
 diactivatePage();
